9ef5750860c9655460415e1513e0463df28a8a53
Henrique <hike.lemes@gmail.com>
2019-10-24 21:51:50 -0300

    modulos de exportação

--- src/main/java/sc/senac/br/cadastropessoa/bean/CadastroUserBean.java
+++ src/main/java/sc/senac/br/cadastropessoa/bean/CadastroUserBean.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
-import javax.enterprise.context.RequestScoped;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
